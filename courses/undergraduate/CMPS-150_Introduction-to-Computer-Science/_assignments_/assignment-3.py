711d5d414a8b419d6c58336117a30fb22a8c0a23
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-03-19 17:13:40 -0500

    Add initial version of CMPS-150_Assignment-3

--- courses/undergraduate/CMPS-150_Introduction-to-Computer-Science/_assignments_/assignment-3.py
+++ courses/undergraduate/CMPS-150_Introduction-to-Computer-Science/_assignments_/assignment-3.py
@@ -12,60 +12,166 @@ Description:                    Determine the cost of a Mardi Gras order. Order 
                 
 Certification of Authenticity:  I certify that this assignment is entirely my own work.
 """
+# Define cake types.
+cake_types:     list =  ["Cinnamon", "Blueberry", "Bavarian"]
+
 # Initialize order entry.
 order_entry:    dict =  {
-    "Cinnamon":     {
-        "quantity": 0,
-        "large":    0
-    },
-    "Blueberry":    {
-        "quantity": 0,
-        "large":    0
-    },
-    "Bavarian":     {
-        "quantity": 0,
-        "large":    0
-    }
-}
+                            cake_type:      {
+                                                "large":    {
+                                                                "quantity": 0,
+                                                                "cost":     0.0
+                                                            },
+                                                "small":    {
+                                                                "quantity": 0,
+                                                                "cost":     0.0
+                                                            },
+                                                "total":    {
+                                                                "quantity": 0,
+                                                                "cost":     0.0
+                                                            }
+                                            }
+                            for cake_type in cake_types
+                        }
 
 # Initialize receipt.
 receipt:    str =   f"""
-------------------------------------------------
-                HAPPY MARDI GRAS
-------------------------------------------------
+==============================================
+               HAPPY MARDI GRAS                 
+==============================================
 
-{"King Cake":10}{"Large":5}{"Small":5}{"Cost":5}
--------------------------------------------------
+{"King Cake":15}{"Large":>10}{"Small":>10}{"Cost":>10}
+----------------------------------------------
 """
 
 # For each type of cake...
 for cake_type in order_entry.keys():
     
     # Prompt for the number of cakes.
-    order_entry[cake_type].update(
-        {"quantity": int(input(f"\nHow many {cake_type:9} cakes would you like? "))}
+    order_entry[cake_type]["total"].update(
+        {"quantity": int(input(f"\nHow many {cake_type} cakes would you like? "))}
     )
     
     # If the number of cakes is greater than zero...
-    if order_entry[cake_type]["quantity"] > 0:
+    if order_entry[cake_type]["total"]["quantity"] > 0:
+        
+        # Add cake type to receipt.
+        receipt +=  f"""\n{cake_type:15}"""
         
         # Prompt for the nuymber that should be large cakes.
-        order_entry[cake_type].update(
-            {"large": int(input(f"And how many of those should be large?   "))}
-        )
+        order_entry[cake_type]["large"].update(
+                                                {
+                                                    "quantity": int(input(f"And how many of those should be large?   "))
+                                                }
+                                            )
         
-# Print receipt.
-print(f"""
-------------------------------------------------
-                HAPPY MARDI GRAS
-------------------------------------------------
+        # Add large quantity to receipt.
+        receipt +=  f"""{order_entry[cake_type]["large"]["quantity"]:>10}"""
+        
+        # Calculate small cake quantity.
+        order_entry[cake_type]["small"].update(
+                                                {
+                                                    "quantity": order_entry[cake_type]["total"]["quantity"] - order_entry[cake_type]["large"]["quantity"]
+                                                }
+                                            )
+        
+        # Add small quantity to receipt.
+        receipt +=  f"""{order_entry[cake_type]["small"]["quantity"]:>10}"""
+        
+        # Calculate large cake costs.
+        order_entry[cake_type]["large"].update(
+                                                {
+                                                    "cost":     (order_entry[cake_type]["large"]["quantity"] * 24.99)
+                                                }
+                                            )
+        
+        # Calculate small cake costs.
+        order_entry[cake_type]["small"].update(
+                                                {
+                                                    "cost":     (order_entry[cake_type]["small"]["quantity"] * 16.99)
+                                                }
+                                            )
+        
+        # Calculate total costs.
+        order_entry[cake_type]["total"].update(
+                                                {
+                                                    "cost":     (order_entry[cake_type]["large"]["cost"] + order_entry[cake_type]["small"]["cost"])
+                                                }
+                                            )
+        
+        # Add total cost for cake type to receipt.
+        receipt +=  f"""{f"${round(float(order_entry[cake_type]["total"]["cost"]), 2)}":>10}"""
+        
+# Add "Total" section to order entry.
+order_entry.update(
+                            {"Subtotal":    {
+                                            "large":    {
+                                                            "quantity": 0,
+                                                            "cost":     0.0
+                                                        },
+                                            "small":    {
+                                                            "quantity": 0,
+                                                            "cost":     0.0
+                                                        },
+                                            "total":    {
+                                                            "quantity": 0,
+                                                            "cost":     0.0
+                                                        }
+                                        }}
+                        )
 
-King Cake   Large   Small   Cost
--------------------------------------------------
-Cinnamon    
-Blueberry   
-Bavarian    
--------------------------------------------------
-""")
+# Add total large cake quantity to receipt.
+order_entry["Subtotal"]["large"].update(
+                            {
+                                "quantity": sum([order_entry[cake_type]["large"]["quantity"] for cake_type in cake_types])
+                            }
+                        )
+
+# Add total small cake quantity to receipt.
+order_entry["Subtotal"]["small"].update(
+                            {
+                                "quantity": sum([order_entry[cake_type]["small"]["quantity"] for cake_type in cake_types])
+                            }
+                        )
+
+# Add total cost to receipt.
+order_entry["Subtotal"]["total"].update(
+                            {
+                                "cost":     sum([order_entry[cake_type]["total"]["cost"] for cake_type in cake_types])
+                            }
+                        )
+
+# Add line before total on receipt.
+receipt += "\n=============================================="
+
+# Add "Totals" line to receipt.
+receipt +=  f"""\n{"Subtotal":15}{order_entry["Subtotal"]["large"]["quantity"]:>10}{order_entry["Subtotal"]["small"]["quantity"]:>10}{f"${round(float(order_entry["Subtotal"]["total"]["cost"]), 2)}":>10}"""
+
+# Calculate total quantity of cakes.
+order_entry["Subtotal"]["total"].update(
+                            {
+                                "quantity": order_entry["Subtotal"]["large"]["quantity"] + order_entry["Subtotal"]["small"]["quantity"]
+                            }
+                        )
+
+# Add total cake quantity to receipt.
+receipt +=  f"""\nTotal number of king cakes: {order_entry["Subtotal"]["total"]["quantity"]}"""
+
+# If customer ordered more than 7 cakes...
+if order_entry["Subtotal"]["total"]["quantity"] > 7:
     
-print(order_entry)
+    # They receive a 10% discount.
+    order_entry["Subtotal"]["total"].update(
+                                            {
+                                                "discount": order_entry["Subtotal"]["total"]["cost"] * .1
+                                            }
+                                        )
+
+    # Add discount to receipt.
+    receipt +=  f"""\nTotal number of king cakes: {order_entry["Subtotal"]["total"]["discount"]}"""
+    
+# Add bottom border of receipt.
+receipt += "\n=============================================="
+
+# Print final receipt.
+print(receipt)
